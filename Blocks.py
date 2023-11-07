6fdd06df786b13b5dbed4ae06cef467f3aa13495
R0lton <108031948+R0lton@users.noreply.github.com>
2023-11-07 14:41:49 +0300

    Funny changes for Irochka

--- Blocks.py
+++ Blocks.py
@@ -32,7 +32,7 @@ class Cloud(sprite.Sprite):
         sprite.Sprite.__init__(self)
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
         # self.image.fill(Color(PLATFORM_COLOR))
-        self.image = image.load("%s/sprites/blocks/platform2.png" % ICON_DIR)
+        self.image = image.load("%s/sprites/blocks/platform.png" % ICON_DIR)
         self.image.set_colorkey(Color('#FFFFFF'))
 
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
